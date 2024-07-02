9b52de2556febf3cc64f9e5d4f0b45313ef0de28
kelvin.lok <kelvin.lok@greneradar.com>
2024-07-02 07:28:02 +0000

    only dist

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-restricted-exports
-export { default } from './core/SequenceExamWrapper';
-export { default as OuterExamTimer } from './core/OuterExamTimer';
-export {
-  useExamAccessToken,
-  useFetchExamAccessToken,
-  useIsExam,
-} from './api';
-export { reducer } from './data';
