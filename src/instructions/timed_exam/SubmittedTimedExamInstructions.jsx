c17ade7d708c97fce9300ad67fbcb2c53208cae5
kelvin.lok <kelvin.lok@greneradar.com>
2024-07-09 01:58:37 +0000

    fix

--- src/instructions/timed_exam/SubmittedTimedExamInstructions.jsx
+++ src/instructions/timed_exam/SubmittedTimedExamInstructions.jsx
@@ -3,19 +3,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { FormattedMessage } from "@edx/frontend-platform/i18n";
 import { getExamAttemptsData } from "../../data";
 import { Button } from "@openedx/paragon";
+import {
+  fetchLatestAttempt,
+} from '../../api';
 const SubmittedTimedExamInstructions = () => {
   const { timeIsOver, exam, progress } = useSelector(
     (state) => state.specialExams
   );
   const [timeLeft, setTimeLeft] = useState(5);
   const [isPass, setIspass] = useState(false);
+  const [attempt, setAttempt]= useState(true);
   const dispatch = useDispatch();
   console.log(progress);
   console.log(exam);
 
 
 
-  useEffect(() => {
+  useEffect(async () => {
     if (timeLeft === 0) {
       // Countdown has reached zero, do something
       console.log("Countdown finished!");
@@ -34,14 +38,16 @@ const SubmittedTimedExamInstructions = () => {
         }
       }
       if (!isPass) {
-        dispatch(getExamAttemptsData(exam.course_id, exam.content_id));
+       let attempt =  await fetchLatestAttempt(exam.course_id);
+       setAttempt(attempt)
+
       }
       return;
     }
 
     const timer = setTimeout(() => {
       setTimeLeft(timeLeft - 1);
-    }, 500);
+    }, 1000);
 
     return () => {
       clearTimeout(timer);
@@ -58,10 +64,10 @@ const SubmittedTimedExamInstructions = () => {
           />
           <div class="progress">
             <div
-              style={{ width: `${(5 - timeLeft) * 10}%` }}
+              style={{ width: `${(5 - timeLeft) * 20}%` }}
               class="progress-bar"
               role="progressbar"
-              aria-valuenow={(5 - timeLeft) * 10}
+              aria-valuenow={(5 - timeLeft) * 20}
               aria-valuemin="0"
               aria-valuemax="100"
             ></div>
@@ -78,7 +84,7 @@ const SubmittedTimedExamInstructions = () => {
             id="exam.submittedExamInstructions.fail"
             defaultMessage="Unfortunately, you did not pass the exam. Please note that retaking the exam may be necessary based on your organization policy."
           />
-          {!exam?.attempt && (
+          {!attempt && (
             <Button
               variant="outline-primary"
               onClick={() => window.location.reload()}
